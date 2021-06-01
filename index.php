e429a5090e4f2dc8c7c8952fad8c402688d56848
AlejandroYam <alejandro98b182@gmail.com>
2021-06-01 12:24:59 -0500

    pagina de inicio de cipal

---
+++ index.php
@@ -0,0 +1,395 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Cipal | Inicio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="index is-preload">
+		<style>
+			#banner{
+				
+			}
+			#banner{
+				background-repeat: repeat;
+				background-size: cover;
+				background-position:center;
+				width: 100%;
+				height: 100vh;
+				animation:slide 5s infinite;
+				
+			}
+			.bg-blue{
+				background-color: #062045;
+				color:white;
+				position:relative
+			}
+			.no-border{
+				border:0px;
+			}
+			.no-bottom{
+				margin-bottom:0px;
+			}
+			.tin{
+				font-size:15px;
+				text-align:;
+				}
+				.rounded-lg {
+				border-radius: 1rem !important;
+				}
+
+				.text-small {
+				font-size: 0.9rem !important;
+				}
+
+				.custom-separator {
+				width: 5rem;
+				height: 6px;
+				border-radius: 1rem;
+				}
+
+				.text-uppercase {
+				letter-spacing: 0.2em;
+				}
+				#header{
+					position:fixed;
+				}
+			@keyframes slide {
+				0%{
+					background-image: url('images/carousel/carousel1.png');
+				}
+				50%{
+					background-image: url('images/carousel/carousel2.png');
+				}
+				100%{
+					background-image: url('images/carousel/carousel3.png');
+				}
+			}
+		</style>
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt" style="padding-top: 13px;padding-bottom: 0px;">
+				<img width="110" src="images/logo/CIPAL.png" alt="">	
+					<nav id="nav" class="">
+						<ul>
+							<li class="current"><a href="index.html">Inicio</a></li>
+							<li class="current"><a href="index.html">¿Cómo funciona?</a></li>
+							<li class="submenu">
+								<a href="#">Documentación</a>
+								<ul>
+									<li><a href="left-sidebar.html">Instalación</a></li>
+									<li><a href="right-sidebar.html">Métodos</a></li>
+									<li><a href="no-sidebar.html">Ayuda</a></li>
+									<li><a href="contact.html">Contacto</a></li>
+									<li class="submenu">
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#">Instalación</a></li>
+											<li><a href="#">Métodos</a></li>
+											<li><a href="#">Soporte</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li class="current"><a href="index.html">Descargar</a></li>
+							<li class="current"><a href="index.html">Contacto</a></li>
+							<li><a href="#" class="button primary">Ingresar</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+			<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/carousel/carousel1.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="images/carousel/carousel2.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="images/carousel/carousel3.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+			<!-- Main -->
+				<article id="main">
+						<!-- Two -->
+					<section class="wrapper style1 container special">
+						<div class="row">
+							<div class="col-6 col-12-narrower">
+				
+								<section>
+									<header>
+										<h3></h3>
+										<br><br><br>
+									</header>
+									<p style="text-align:left; color:#072146"><strong>CIPAL</strong> es el software de facturación municipal, propiedad de <strong>CFactura</strong> y diseñado por nuestro
+										equipo de expertos desarrolladores, destinado las alacaldías municipales. Es las solución para 
+										automatización del proceso de finanzas en las administraciones municipales.
+									</p>
+								</section>
+				
+							</div>
+							<div class="col-6 col-12-narrower">
+				
+								<section>
+									<header>
+										<h3></h3>
+									</header>
+									<img height="400" src="images/info/sit-pc.png" alt="">
+								</section>
+				
+							</div>
+				
+						</div>
+					</section>
+					<!-- One -->
+					<section class="wrapper style2 container special-alt">
+						<div class="row gtr-50">
+
+							<div class="col-6 col-12-narrower imp-narrower">
+
+								<ul class="featured-icons">
+									<img height="210" src="images/iconos/descarga-pc.png" alt="">
+								</ul>
+
+							</div>
+
+							<div class="col-6 col-12-narrower">
+
+								<header>
+									<h2>Descarga ahora el software</h2>
+								</header>
+								<footer>
+									<ul class="buttons">
+										<li><a href="descarga.php" class="button">Descargar</a></li>
+									</ul>
+								</footer>
+
+							</div>
+						</div>
+					</section>
+						<!--------------main------------
+					<header class="special container">
+						<span class="icon solid fa-chart-bar"></span>
+						<h2>As this is my <strong>twentieth</strong> freebie for HTML5 UP
+						<br />
+						I decided to give it a really creative name.</h2>
+						<p>Turns out <strong>Twenty</strong> was the best I could come up with. Anyway, lame name aside,
+						<br />
+						it's minimally designed, fully responsive, built on HTML5/CSS3,
+						and, like all my stuff,
+						<br />
+						released for free under the <a href="http://html5up.net/license">Creative Commons Attribution 3.0</a> license. Have fun!</p>
+					</header>
+
+				-->
+					<!-- Three -->
+					<section>
+  <div class="container py-5">
+
+						<!-- FOR DEMO PURPOSE -->
+						<!-- END -->
+
+
+
+						<div class="row text-center align-items-end">
+						<!-- Pricing Table-->
+						<div class="col-md-6 mb-5" style="padding-left: 15px;">
+							<div class="bg-white p-5 rounded-lg shadow">
+							<h1 class="h6 text-uppercase font-weight-bold mb-4">Nombre: Ekca Consultores Fiscales SCP</h1>
+							<h5 class="h1 font-weight-bold">Depósito en: </h5>
+							<img width="190" src="images/iconos/oxxo.png" alt="">
+
+							<div class="custom-separator my-4 mx-auto bg-primary"></div>
+
+							<ul class="list-unstyled my-5 text-small text-left">
+							<br>
+								<li class="mb-3">
+								<i class="fa fa-check mr-2 text-primary"></i> Número de cuenta: </li>
+								<li class="mb-3">
+								<i class="fa fa-check mr-2 text-primary"></i> 5204-1671-2765-0794</li>
+								<br>
+								<li> </li>
+							</ul>
+							<a href="#" class="btn btn-primary btn-block p-2 shadow rounded-pill">Comprar</a>
+							</div>
+						</div>
+						<!-- END -->
+
+
+						<!-- Pricing Table-->
+						<div class="col-md-6 mb-5" style="padding-left: 15px;">
+							<div class="bg-white p-5 rounded-lg shadow">
+							<h1 class="h6 text-uppercase font-weight-bold mb-4">Nombre: Ekca Consultores Fiscales SCP</h1>
+							<h5 class="h1 font-weight-bold">Depósito en: </h5>
+							<img width="160" src="images/iconos/banamex.png" alt="">
+							<div class="custom-separator my-4 mx-auto bg-primary"></div>
+
+							<ul class="list-unstyled my-5 text-small text-left font-weight-normal">
+								<li class="mb-3">
+								<i class="fa fa-check mr-2 text-primary"></i> RFC: 7376988</li>
+								<li class="mb-3">
+								<i class="fa fa-check mr-2 text-primary"></i> NUM SUC: 7004</li>
+								<li class="mb-3">
+								<i class="fa fa-check mr-2 text-primary"></i> Clave interbancaria</li>
+								<li class="mb-3">
+								<i class="fa fa-check mr-2 text-primary"></i> 002910700473769887</li>
+							</ul>
+							<a href="#" class="btn btn-primary btn-block p-2 shadow rounded-pill">Comprar</a>
+							</div>
+						</div>
+						<!-- END -->
+
+
+						<!-- Pricing Table-->
+					
+						<!-- END -->
+
+						</div>
+					</div>
+					</section>
+						<section class="wrapper style1 container special">
+						<div class="row" style="text-align:left">
+							<div class="col-6 col-12-narrower">
+				
+								<section>
+									<header>
+										<h3 class="no-bottom">Nuestra oficina</h3>
+										<p style="font-size:18px; margin-bottom:20px;">Ponte en contacto con nosotros o puedes visitarnos en nuestras oficinas </p>
+										<h3 class="no-bottom">Dirección</h3>
+										<p style="font-size:18px; margin-bottom:20px;">Calle 1 Num. 160 x 16 y 18 Fracc. Mulsay, Merida, Yucatan CP9 7246</p>
+										<h3 class="no-bottom">Email</h3>
+										<p style="font-size:18px; margin-bottom:20px;">info@cfactura.com.mx</p>
+										<h3 class="no-bottom">Tel.</h3>
+										<p style="font-size:18px; margin-bottom:20px;">(999) 167-10-35 | (999) 290-36-78</p>
+										<h3 class="no-bottom">Celular</h3>
+										<p style="font-size:18px; margin-bottom:20px;">999-256-79-09</p>
+									</header>
+
+								</section>
+				
+							</div>
+							<div class="col-6 col-12-narrower">
+				
+								<section>
+									<header>
+										<h3></h3>
+									</header>
+									<div style="width: 100%"><iframe width="100%" height="470" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=calle%201%20numero%20150+(CFactura)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+								</section>
+				
+							</div>
+				
+						</div>
+					</section>
+
+				</article>
+
+			<!-- CTA ---
+				<section id="cta">
+
+					<header>
+						<h2>Ready to do <strong>something</strong>?</h2>
+						<p>Proin a ullamcorper elit, et sagittis turpis integer ut fermentum.</p>
+					</header>
+					<footer>
+						<ul class="buttons">
+							<li><a href="#" class="button primary">Take My Money</a></li>
+							<li><a href="#" class="button">LOL Wut</a></li>
+						</ul>
+					</footer>
+
+				</section>
+----->
+
+			<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted" >
+  <!-- Section: Social media -->
+
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class=""style="background-color:#072146;">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+          <!-- Content -->
+			<img width="200" src="images/iconos/cfactura.png" alt="">
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+		<p class="tin"><i style="color:#02a5a5;"class="fas fa-map-marker-alt"></i> Calle 1 Num. 160 x 16 y 18 Fracc. Mulsay,<br>
+               Mérida, Yucatán CP.97246.</p>
+            <p class="tin"><i style="color:#02a5a5;" class="fas fa-phone-alt"></i> Tel.:(999) 290-36-78</p>
+            <p class="tin"><i style="color:#02a5a5;" class="far fa-envelope"></i> info@cfactura.com.mx</p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+          <!-- Links -->
+		  <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a width="100" href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+        </div>
+        <!-- Grid column -->
+        <!-- Grid column -->
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+</footer>
+<!-- Footer -->
+
+		</div>
+		<!-- Scripts -->
+
+		
+
+	</body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+</html>
